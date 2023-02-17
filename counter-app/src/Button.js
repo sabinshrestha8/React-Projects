5839f7addd32cdd2754e777b0a5c6ab43e0dba5a
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-18 00:59:25 +0545

    Optimize switch-case with valueMap object making code concise

--- counter-app/src/Button.js
+++ counter-app/src/Button.js
@@ -2,18 +2,24 @@ const Button = ({counter, setCounter}) => {
     const buttons = ['+', 'reset', '-'];
 
     const handleCount = (e) => {
-        switch (e.target.value) {
-          case '+':
-            setCounter(counter + 1);
-            break;
-          case '-':
-            counter >= 1 ? setCounter(counter - 1) : setCounter(0);
-            break;
-          default:
-            setCounter(0);
-            break;
-        }
-    };
+        /*  `valueMap` is an object that has +, - and reset as keys &
+        each key has a value that corresponds to what the counter should
+        be updated to when the respective button is clicked. */
+        const valueMap = {
+          '+': counter + 1,
+          '-': counter >= 1 ? counter - 1 : 0,
+          'reset': 0
+        };
+        
+        /* So, valueMap[e.target.value] will retrieve the corresponding new value of
+        the counter from the valueMap object based on the value of the clicked button.
+        Finally, setCounter() is called with the new value to update the state of 
+        the counter. */
+        setCounter(valueMap[e.target.value]);
+
+        /* Note: The bracket notation is useful when we need to access properties 
+        dynamically, using a variable or an expression inside the brackets. */
+      };      
 
     return (
         <div className="btn">
