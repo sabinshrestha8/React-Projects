fc0ef3ac55da9af9484ffb1c52e0d3bbdf8bbc0a
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-03-01 06:13:46 +0545

    Added Select & Update functionality on todolist

--- myTodo/src/components/Create.jsx
+++ myTodo/src/components/Create.jsx
@@ -1,29 +1,65 @@
-import { useState } from "react";
-
-const Create = ({ handleAddTodo }) => {
-    const [title, setTitle] = useState("");
-    // const [iscompleted, setIsCompleted] = useState(false);
+const Create = ({ handleAddTodo, setTitle, title, index, setIndex, data, setData, iscompleted }) => {
 
     const handleSubmit = (e) => {  
         e.preventDefault();
-        const todo = { title, iscompleted: false };
-        
-        fetch('http://localhost:8000/todos', {
-            method: 'post',
-            headers: { "content-Type": "application/json" },
-            body: JSON.stringify(todo)
-        })
-        .then(
-            res => res.json()
-        )
-        .then(
-            (data) => {
-                // console.log(data);
-                handleAddTodo(data);
+
+        const todo = { title, iscompleted };
+
+        if (index !== null) {
+            fetch('http://localhost:8000/todos/' + data[index].id, {
+                method: 'PATCH',
+                headers: { "content-Type": "application/json" },
+                body: JSON.stringify({...data[index], title})
+            })
+            .then(
+                res => res.json()
+            )
+            .then((newTodo) => {
+                // const editTodo = data.map((todo) => {
+                //     if (todo.id === data[index].id) {
+                //         // todo.title = title
+                //         return {...todo, title: title}
+                //     }
+
+                //     return todo;
+                // })
+
+                const editedTodo = data.map((todo) => {
+                    if (todo.id === newTodo.id) {
+                        return {...todo, title: title};
+                    }
+
+                    return todo;
+                })
+
+                setData(editedTodo);
+
+                // setData(...data, {...newTodo, title: title});
+
+                // console.log({...newTodo, title: title});
+
+                setIndex(null);
                 setTitle('');
-                console.log("todo added");
-            }
-        )
+                console.log('todo updated');
+            });
+        } else {
+            fetch('http://localhost:8000/todos', {
+                method: 'post',
+                headers: { "content-Type": "application/json" },
+                body: JSON.stringify(todo)
+            })
+            .then(
+                res => res.json()
+            )
+            .then(
+                (data) => {
+                    // console.log(data);
+                    handleAddTodo(data);
+                    setTitle('');
+                    console.log("todo added");
+                }
+            )
+        }
     }
 
     return (
@@ -38,7 +74,6 @@ const Create = ({ handleAddTodo }) => {
                     required
                 />
             </form>
-            {/* <p>{ title }</p> */}
         </div>
     );
 }
