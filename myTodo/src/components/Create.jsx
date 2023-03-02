9a80732893e30331769984156864f0746ccc71bc
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-03-02 15:31:38 +0545

    Added conditionals to input

--- myTodo/src/components/Create.jsx
+++ myTodo/src/components/Create.jsx
@@ -3,66 +3,68 @@ const Create = ({ handleAddTodo, setTitle, title, index, setIndex, data, setData
     const handleSubmit = (e) => {  
         e.preventDefault();
 
-        const todo = { title, iscompleted: false };
+        if (title.trim()) {
+            const todo = { title, iscompleted: false };
 
-        if (index) {
-            fetch('http://localhost:8000/todos/' + data[index].id, {
-                method: 'PATCH',
-                headers: { "content-Type": "application/json" },
-                body: JSON.stringify({...data[index], title})
-            })
-            .then(
-                res => {
-                    if (!res.ok) {
-                        throw new Error('Failed to update todo');
-                    }
-
-                    return res.json();
-                }   
-            )
-            .then((newTodo) => {
-                const editedTodo = data.map((todo) => {
-                    if (todo.id === newTodo.id) {
-                        return {...todo, title: title};
-                    }
-
-                    return todo;
+            if (index !== null) {
+                fetch('http://localhost:8000/todos/' + data[index].id, {
+                    method: 'PATCH',
+                    headers: { "content-Type": "application/json" },
+                    body: JSON.stringify({...data[index], title})
                 })
+                .then(
+                    res => {
+                        if (!res.ok) {
+                            throw new Error('Failed to update todo');
+                        }
 
-                setData(editedTodo);
-                setIndex(null);
-                setTitle('');
-                console.log('todo updated');
-            })
-            .catch(error => console.log(error))
-            .finally(
-                () => {
+                        return res.json();
+                    }   
+                )
+                .then((newTodo) => {
+                    const editedTodo = data.map((todo) => {
+                        if (todo.id === newTodo.id) {
+                            return {...todo, title: title};
+                        }
+
+                        return todo;
+                    })
+
+                    setData(editedTodo);
+                    setIndex(null);
                     setTitle('');
-                }
-            );
-        } else {
-            fetch('http://localhost:8000/todos', {
-                method: 'post',
-                headers: { "content-Type": "application/json" },
-                body: JSON.stringify(todo)
-            })
-            .then(
-                res => {
-                    if (!res.ok) {
-                        throw new Error('Failed to add todo');
+                    console.log('todo updated');
+                })
+                .catch(error => console.log(error))
+                .finally(
+                    () => {
+                        setTitle('');
                     }
+                );
+            } else {
+                fetch('http://localhost:8000/todos', {
+                    method: 'post',
+                    headers: { "content-Type": "application/json" },
+                    body: JSON.stringify(todo)
+                })
+                .then(
+                    res => {
+                        if (!res.ok) {
+                            throw new Error('Failed to add todo');
+                        }
 
-                    return res.json();
-                }      
-            )
-            .then(
-                data => {
-                    handleAddTodo(data);
-                    setTitle('');
-                    console.log("todo added");
-                }
-            )
-            .catch(error => console.log(error));
+                        return res.json();
+                    }      
+                )
+                .then(
+                    data => {
+                        handleAddTodo(data);
+                        setTitle('');
+                        console.log("todo added");
+                    }
+                )
+                .catch(error => console.log(error));
+            }
         }
     }
 
@@ -75,7 +77,6 @@ const Create = ({ handleAddTodo, setTitle, title, index, setIndex, data, setData
                     value={ title }
                     onChange={ e => setTitle(e.target.value) }
                     placeholder="add todo"
-                    required
                 />
             </form>
         </div>
