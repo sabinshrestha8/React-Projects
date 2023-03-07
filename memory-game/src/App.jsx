5b7799daa463e9ed79f7217ebe443ac8f8e81595
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-03-07 11:41:32 +0545

    #4-Creating a card component with its associated css file

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import SingleCard from './components/SingleCard'
 
 const cardImages = [
   { "src": "/img/helmet-1.png" },
@@ -34,12 +35,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <div className="card" key={card.id}>
-            <div>
-              <img src={card.src} alt="card front" className="front" />
-              <img src="/img/cover.png" alt="card back" className="back" />
-            </div>
-          </div>
+          <SingleCard card={card} key={card.id} />
         ))}
       </div>
     </div>
