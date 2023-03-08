4a01b3b0293d163e29a3955688856caf9208da39
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-03-08 18:24:01 +0545

    #7-Update matched cards obj property to true

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" }
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ]
 
 function App() {
@@ -38,16 +38,25 @@ function App() {
 
   // compare 2 selected cards
   useEffect(() => {
-      if (choiceOne && choiceTwo) {
-        if (choiceOne.src === choiceTwo.src) {
-          console.log('cards matched')
-          resetTurn()
-        } else {
-          console.log('cards not matched')
-          resetTurn()
-        }      
-      }
-    }, [choiceOne, choiceTwo])
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }      
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards);
 
   // handle a choice
   function handleChoice(card) {
