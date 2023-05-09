1b9127b11c70f073b8b1b5f025259d898df3ad22
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-05-09 17:44:27 +0545

    Create list group component & wrap it with fragments

--- react-with-typescript/src/Message.tsx
+++ react-with-typescript/src/Message.tsx
@@ -3,9 +3,9 @@ function Message() {  // In react application whenever you create a function com
     // JSX: JavaScript XML
     const name = 'Mosh';
 
-    if (name)
+    // if (name)
         return <h1>Hello { name }</h1>;
-    return <h1>Hello World</h1>;
+    // return <h1>Hello World</h1>;
 }
 
 export default Message;
