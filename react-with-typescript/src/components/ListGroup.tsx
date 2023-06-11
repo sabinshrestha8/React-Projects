c7f494b3c28ff7c25be6b2404a85ec8dac1da2e6
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-06-11 15:23:15 +0545

    Handling events on react using typescript

--- react-with-typescript/src/components/ListGroup.tsx
+++ react-with-typescript/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
     let items = [
         'New York',
@@ -7,15 +9,24 @@ function ListGroup() {
         'Paris' 
     ];
 
-    items = [];
+    // To create an event handler fn, by convention we start with the word handle & then we specify the type of event
+    const handleClick = (event: MouseEvent) => { /* Using `Type Annotation` to specify the type of event param */
+        console.log(event);
+    };
 
     return (
         <>
             <h1>List</h1>   {/* In react a component cannot return more than one element. To solve this problem we can wrap these markup inside <div> or fragment(<>). */}
             { items.length === 0  && <p>No item found</p> }
             <ul className="list-group"> 
-                {items.map((item) => (
-                    <li key={item} className="list-group-item">{item}</li>
+                {items.map((item, index) => ( // when mapping items we can opt add index as 2nd param & with this we can log index of an item that was clicked.
+                    <li key={item} 
+                        className="list-group-item" 
+                        // onClick={(event) => console.log(item, index, event)}
+                        onClick={handleClick}
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
         </>
