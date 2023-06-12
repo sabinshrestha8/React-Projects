9e2c16f73439548f069a691317542fdc3e00fd0d
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-06-13 00:31:02 +0545

    Passing data via props in react using typescript

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,8 +1,16 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = [
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris' 
+  ];
+
   // In react, Each component has it's own state. So they will be independent of each other. For example: Each `ListGroup` is going to have it's own state.
-  return <div><ListGroup /></div>;
+  return <div><ListGroup heading="Cities" items={items} /></div>;
 }
 
 export default App;
--- react-with-typescript/src/components/ListGroup.tsx
+++ react-with-typescript/src/components/ListGroup.tsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
-function ListGroup() {
-    let items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris' 
-    ];
+// Define structure/shape of an object i.e. { heading: string, items: [] }
+interface Props {
+    heading: string;
+    items: string[];
+}
 
+// function ListGroup(props: Props)
+function ListGroup({ items, heading }: Props) { // destructuring the props parameter
     // useState() hook returns an array containing two elements which is then destructured as below:
     let [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-            <h1>List</h1>   {/* In react a component cannot return more than one element. To solve this problem we can wrap these markup inside <div> or fragment(<>). */}
+            <h1>{heading}</h1>   {/* In react a component cannot return more than one element. To solve this problem we can wrap these markup inside <div> or fragment(<>). */}
             { items.length === 0  && <p>No item found</p> }
             <ul className="list-group"> 
                 {items.map((item, index) => ( // when mapping items we can opt add index as 2nd param & with this we can log index of an item that was clicked.
